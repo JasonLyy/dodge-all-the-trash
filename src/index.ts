44a4d0d63f787610677c51c6cfec627a9190f8b7
Jason <ly.jason.1999@gmail.com>
2020-04-21 23:54:26 +1000

    fixed credentials file for local dev

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@ import * as dotenv from 'dotenv';
 import * as gcpMetadata from 'gcp-metadata';
 import messageProcessor from './msgProcessor';
 import { Firestore } from '@google-cloud/firestore';
-dotenv.config({ path: resolve(__dirname, './.env') });
+dotenv.config({ path: resolve(__dirname, '../.env') });
 
 const initialiseFireStore = (): Firestore => {
     // Invoked from non google machine
     if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
-        process.env.GOOGLE_APPLICATION_CREDENTIALS = `${resolve(__dirname, './credentials.json')}`;
+        process.env.GOOGLE_APPLICATION_CREDENTIALS = resolve(__dirname, './credentials.json');
     }
 
     admin.initializeApp({
